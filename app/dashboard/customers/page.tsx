acea8dc3857fbc5bd609f8776d15475cd8b9bfce
PublicVoidMarx <luchigroso9@hotmail.com>
2023-11-17 20:29:04 +1100

    primeras pantallas

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
-const customers = () => {
+export default function page() {
   return (
     <div>customers</div>
   )
 }
-
-export default customers
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import SideNav from '../ui/dashboard/sidenav'
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
       <div className='w-full flex-none md:w-64'>
@@ -11,5 +11,3 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     </div>
   )
 }
-
-export default Layout
