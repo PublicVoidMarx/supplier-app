95b4e0eddb4c8b067b15050abf5ec731117ed98a
PublicVoidMarx <luchigroso9@hotmail.com>
2023-11-18 15:56:31 +1100

    authentification

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import { signIn } from '@/auth'
 
 const formSchema = z.object({
   id: z.string(),
@@ -87,5 +88,16 @@ export async function deleteInvoice(id: string) {
     return { message: 'Deleted Invoice.' };
   } catch (error) {
     return { message: 'Database Error: Failed to Delete Invoice.' }
+  }
+}
+
+export async function authenticate(prevState: string | undefined, formData: FormData,) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData))
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialsSignin';
+    }
+    throw error;
   }
 }
